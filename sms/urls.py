a7939a68a02453b5a3f44f2c5c87647615b79ebc
Adam Bogdal <adam@bogdal.pl>
2013-02-09 01:02:09 +0100

    Remove unnecessary path to view

--- sms/urls.py
+++ sms/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from sms.decorators import ip_restrictions
 from sms.views import callback_received_sms, callback_delivery_report
 
-urlpatterns = patterns('sms.views',
+urlpatterns = patterns('',
     url(r'^callback/received-sms/$', ip_restrictions(callback_received_sms), name='callback-received-sms'),
     url(r'^callback/delivery-report/$', ip_restrictions(callback_delivery_report), name='callback-delivery-report'),
 )
